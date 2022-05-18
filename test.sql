ce739f34be798b2f8d6daa4c691a0a121d8145e0
Murad734556 <adylovmurad@gmail.com>
2022-05-18 18:04:57 +0600

    test

--- test.sql
+++ test.sql
@@ -1,1 +1,27 @@
-SELECT * FROM citizens;
+-- 1 задание
+SELECT * FROM auto WHERE make ='Toyota' AND model='Camry' ORDER BY DESC year;
+
+-- 2 задание
+SELECT DISTINCT first_name FROM call_center ORDER BY DESC first_name LIMIT 10;
+
+-- 3 задание
+UPDATE auto SET make='Mercedes' TO 'Mersus';
+
+-- 4 задание
+
+DELETE * FROM drivers WHERE first_name ='Азамат' AND last_name='Азаматов';
+
+-- 5 задание
+
+SELECT COUNT(first_name) FROM drivers WHERE experience > 10 AND gender='female';
+
+
+-- 6 задание
+SELECT AVG(experience) FROM drivers WHERE date_of_birth > '1975-10-10';
+
+-- 7 задание
+SELECT * FROM drivers JOIN auto ON drivers.id_car = auto.id;
+
+-- 8 задание
+
+SELECT COUNT('Toyota'), COUNT('BMW'), COUNT('Mercedes') FROM auto ORDER BY DESC;
--- test2.py
+++ test2.py
@@ -1,1 +1,54 @@
-print("Hello world")
+# # 1задание 
+class KgToPounds:
+    def __init__(self,kg):
+        self.__kg=kg
+
+    def to_pounds(self):
+        return self.__kg * 2.205
+
+    def set_kg(self, new_kg):
+        if isinstance(new_kg, (int, float)):
+            self.__kg = new_kg
+        else:
+            raise ValueError("Киллограммы задаются только числами") 
+
+    def get_kg(self):
+        return self.__kg
+
+    def property(self):
+        kg=property(get_kg, set_kg )
+
+kg=KgToPounds(12)
+
+
+
+# # 2 задание
+class Math:
+    def __init__(self, a, b):
+        self.a=a
+        self.b= b
+    def __add__(self):
+        return self.a + self.b
+
+    def __sub__(self):
+        return self.a - self.b
+
+mat=Math(12,2)
+print(mat.__sub__())
+
+# 3 задание
+
+class Soda:
+    def __init__(self, dobavka):
+        self.dobavka = dobavka
+
+    def show_my_drink(self):
+        if self.dobavka == self.dobavka:
+            print(f"Газировка и {self.dobavka}")
+        else:
+            print(f"Обычная газировка")
+
+sod=Soda("лимон")
+print(sod.show_my_drink())
+         
+
